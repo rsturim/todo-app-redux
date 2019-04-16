99bf56eb610ffe226f99b1e04d72e8ef9ce15cb4
Rich Sturim <rich.sturim@coxautoinc.com>
2019-04-16 06:58:17 -0400

    new stuff

--- src/components/tests/todoList.test.js
+++ src/components/tests/todoList.test.js
@@ -1,20 +1,65 @@
 import React from 'react';
 
+import { Provider } from 'react-redux';
+import configureMockStore from 'redux-mock-store';
+
 import { MemoryRouter } from 'react-router-dom';
-import { mount } from 'enzyme';
 
-import TodoList from '../TodoList';
+import { mount, shallow } from 'enzyme';
+import { TodoList } from '../TodoList';
 
-describe('Name of the group', () => {
-    test('should ', () => {
-        const wrapper = mount(
-            <MemoryRouter>
-                <TodoList />
-            </MemoryRouter>,
-        );
+// import TodoList from '../TodoList';
 
-        console.log(wrapper.debug());
+const mockStore = configureMockStore();
+const store = mockStore({});
 
-        expect(true).toBe(true);
+const mockTodos = [
+    { id: 1, name: 'Load data into app', isComplete: true },
+    { name: 'feed goldfish', isComplete: true, id: 10 },
+    { name: 'shop for candy', isComplete: true, id: 11 },
+    { name: 'buy goldfish', isComplete: false, id: 12 },
+    { name: 'shop for golf balls', isComplete: false, id: 13 },
+    { name: 'purchase concert tickets', isComplete: false, id: 14 },
+];
+
+describe('Testpage Component', () => {
+    const wrapper = shallow(
+        <Provider store={store}>
+            <TodoList />
+        </Provider>,
+        {
+            context: {
+                todos: mockTodos,
+            },
+        },
+    );
+
+    it('should render without throwing an error', () => {
+        // console.log(wrapper.debug());
+
+        expect(wrapper.find('.Todo-List').length).toEqual(1);
+
+        // expect(
+        //     shallow(
+        //         <Provider store={store}>
+        //             <TodoList />
+        //         </Provider>,
+        //     ).exists(<h1>Test page</h1>),
+        // ).toBe(true);
     });
 });
+
+// describe('Name of the group', () => {
+//     test('should ', () => {
+//         const wrapper = mount(
+//             <MemoryRouter>
+//                 <TodoList />
+//             </MemoryRouter>,
+//             { context: { store } },
+//         );
+
+//         console.log(wrapper.debug());
+
+//         expect(true).toBe(true);
+//     });
+// });
