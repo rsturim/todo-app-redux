0bea85bd7f539f909fe570b41984c6513e81fc63
Rich Sturim <rich.sturim@coxautoinc.com>
2019-04-16 06:36:51 -0400

    testing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default props => (
-    <div>
-        <Link to="/">All</Link>
-        <Link to="/active">Active</Link>
-        <Link to="/completed">Completed</Link>
-    </div>
-);
+export default function Footer() {
+    return (
+        <div>
+            <strong>filter by: </strong>
+            <Link to="/">All</Link>
+            <Link to="/active">Active</Link>
+            <Link to="/completed">Completed</Link>
+        </div>
+    );
+}
--- src/components/tests/todoList.test.js
+++ src/components/tests/todoList.test.js
@@ -1,5 +1,20 @@
+import React from 'react';
+
+import { MemoryRouter } from 'react-router-dom';
+import { mount } from 'enzyme';
+
+import TodoList from '../TodoList';
+
 describe('Name of the group', () => {
     test('should ', () => {
+        const wrapper = mount(
+            <MemoryRouter>
+                <TodoList />
+            </MemoryRouter>,
+        );
+
+        console.log(wrapper.debug());
+
         expect(true).toBe(true);
     });
 });
